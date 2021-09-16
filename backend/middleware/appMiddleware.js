78b57b385ad47892f3c1ed541c41031f7d1fcf46
Mohd Ahmed Abdus <mdahmed.abdus@gmail.com>
2021-09-16 20:28:58 +0530

    Add cookie-parser

--- backend/middleware/appMiddleware.js
+++ backend/middleware/appMiddleware.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
 const { IN_PROD } = require('../config/appConfig');
 
 module.exports = app => {
   // body parser
   app.use(express.json());
+  app.use(cookieParser());
 
   if (!IN_PROD) {
     app.use(morgan('dev'));
