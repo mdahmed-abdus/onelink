dabd36e58d17e36e2c0696f2b09d6cafe5f786f7
Mohd Ahmed Abdus <mdahmed.abdus@gmail.com>
2021-09-18 18:42:08 +0530

    Complete links router

--- backend/routes/links.js
+++ backend/routes/links.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const { User } = require('../models/User');
 const { Link } = require('../models/Link');
 const { auth } = require('../middleware/authMiddleware');
+const { NotFound } = require('../errors/customErrors');
 
 const router = express.Router();
 
@@ -14,6 +16,37 @@ router.post('/new', auth, async (req, res) => {
   await user.save();
 
   res.json({ success: true, message: 'New link added', link });
+});
+
+router.patch('/:id', auth, async (req, res) => {
+  const user = await User.findById(req.user.userId);
+  const link = user.links.find((l, i) => l._id.toString() === req.params.id);
+
+  if (!link) {
+    throw new NotFound(`Note with id "${req.params.id}" was not found`);
+  }
+
+  const { title = link.title, url = link.url } = req.body;
+
+  link.title = title;
+  link.url = url;
+  await user.save();
+
+  res.json({ success: true, message: 'Link updated', link });
+});
+
+router.delete('/:id', auth, async (req, res) => {
+  const user = await User.findById(req.user.userId);
+  const link = user.links.find((l, i) => l._id.toString() === req.params.id);
+
+  if (!link) {
+    throw new NotFound(`Note with id "${req.params.id}" was not found`);
+  }
+
+  await link.remove();
+  await user.save();
+
+  res.json({ success: true, message: 'Link deleted', link });
 });
 
 module.exports = router;
