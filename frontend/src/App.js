68da12ec1fbe96d0928d3d85f65b13f78ef09ef1
Mohd Ahmed Abdus <mdahmed.abdus@gmail.com>
2021-09-18 19:31:18 +0530

    Add Home component and react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
