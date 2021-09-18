38e65bf30b25fbe4e41d188c420f24c9e1ae12b1
Mohd Ahmed Abdus <mdahmed.abdus@gmail.com>
2021-09-18 18:48:33 +0530

    Add middleware to links router

--- backend/routes/links.js
+++ backend/routes/links.js
@@ -2,51 +2,65 @@ const express = require('express');
 const { User } = require('../models/User');
 const { Link } = require('../models/Link');
 const { auth } = require('../middleware/authMiddleware');
+const catchAsyncErr = require('../middleware/catchAsyncErr');
+const validateId = require('../middleware/validateId');
 const { NotFound } = require('../errors/customErrors');
 
 const router = express.Router();
 
-router.post('/new', auth, async (req, res) => {
-  const { title, url } = req.body;
+router.post(
+  '/new',
+  auth,
+  catchAsyncErr(async (req, res) => {
+    const { title, url } = req.body;
 
-  const user = await User.findById(req.user.userId);
+    const user = await User.findById(req.user.userId);
 
-  const link = new Link({ title, url });
-  user.links.push(link);
-  await user.save();
+    const link = new Link({ title, url });
+    user.links.push(link);
+    await user.save();
 
-  res.json({ success: true, message: 'New link added', link });
-});
+    res.json({ success: true, message: 'New link added', link });
+  })
+);
 
-router.patch('/:id', auth, async (req, res) => {
-  const user = await User.findById(req.user.userId);
-  const link = user.links.find((l, i) => l._id.toString() === req.params.id);
+router.patch(
+  '/:id',
+  [auth, validateId],
+  catchAsyncErr(async (req, res) => {
+    const user = await User.findById(req.user.userId);
+    const link = user.links.find((l, i) => l._id.toString() === req.params.id);
 
-  if (!link) {
-    throw new NotFound(`Note with id "${req.params.id}" was not found`);
-  }
+    if (!link) {
+      throw new NotFound(`Note with id "${req.params.id}" was not found`);
+    }
 
-  const { title = link.title, url = link.url } = req.body;
+    const { title = link.title, url = link.url } = req.body;
 
-  link.title = title;
-  link.url = url;
-  await user.save();
+    link.title = title;
+    link.url = url;
+    await user.save();
 
-  res.json({ success: true, message: 'Link updated', link });
-});
+    res.json({ success: true, message: 'Link updated', link });
+  })
+);
 
-router.delete('/:id', auth, async (req, res) => {
-  const user = await User.findById(req.user.userId);
-  const link = user.links.find((l, i) => l._id.toString() === req.params.id);
+router.delete(
+  '/:id',
+  [auth, validateId],
+  catchAsyncErr(async (req, res) => {
+    const user = await User.findById(req.user.userId);
+    const link = user.links.find((l, i) => l._id.toString() === req.params.id);
 
-  if (!link) {
-    throw new NotFound(`Note with id "${req.params.id}" was not found`);
-  }
+    if (!link) {
+      throw new NotFound(`Note with id "${req.params.id}" was not found`);
+    }
 
-  await link.remove();
-  await user.save();
+    await link.remove();
+    await user.save();
 
-  res.json({ success: true, message: 'Link deleted', link });
-});
+    res.json({ success: true, message: 'Link deleted', link });
+  })
+);
 
 module.exports = router;
