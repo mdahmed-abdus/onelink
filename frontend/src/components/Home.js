2af7141f96429cc394a00b4fa4368d9004c0138e
Mohd Ahmed Abdus <mdahmed.abdus@gmail.com>
2023-07-14 19:12:29 +0530

    Fix home page ui

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -7,8 +7,10 @@ function Home() {
       <div className="gridContainer">
         <div className="mt-60 grid grid-cols-1 lg:grid-cols-2 gap-10">
           <div className="text-center lg:text-start">
-            <h1 className="text-7xl">Onelink</h1>
-            <p className="text-2xl">A Linktree clone built using MERN Stack</p>
+            <h1 className="text-7xl font-medium">Onelink</h1>
+            <p className="text-2xl font-light">
+              A Linktree clone built using MERN Stack
+            </p>
           </div>
           <div className="flex justify-center lg:justify-end">
             <form className="flex flex-col items-center">
