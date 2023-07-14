fd863f3fa30548191210190eacce615ce2b6d08f
Mohd Ahmed Abdus <mdahmed.abdus@gmail.com>
2023-07-14 22:57:30 +0530

    Fix navbar style

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -31,7 +31,7 @@ function Navbar() {
               </li>
             ))}
           </ul>
-          <div className="sm:hidden md:hidden flex flex-1 justify-end items-center">
+          <div className="sm:hidden flex flex-1 justify-end items-center">
             <img
               src={toggle ? close : menu}
               alt="menu"
