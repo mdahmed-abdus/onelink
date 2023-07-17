e194ba30d4e93ebae088bbcbb79da0c6bf5fcc0a
Mohd Ahmed Abdus <mdahmed.abdus@gmail.com>
2023-07-17 17:43:25 +0530

    Add update form

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -3,6 +3,8 @@ import { useState } from 'react';
 import Link from './Link';
 import BlurredBgCenteredItems from './BlurredBgCenteredItems';
 import TextInput from './TextInput';
+import IconButton from './IconButton';
+import { edit } from '../assets';
 
 function ConfirmRedirect({ url, setShowRedirect }) {
   return (
@@ -27,36 +29,41 @@ function ConfirmRedirect({ url, setShowRedirect }) {
   );
 }
 
-function AddNewLink({ setShowAddNewLink }) {
+function LinkForm({ link, setLink, formFor, closeAllFormView }) {
+  let heading = 'Enter link details';
+
+  if (formFor === 'update') {
+    heading = 'Update link details';
+  }
+
   return (
     <BlurredBgCenteredItems
       items={
         <form className="flex flex-col p-6">
-          <h2 className="text-2xl">Enter link details</h2>
+          <h2 className="text-2xl">{heading}</h2>
           <TextInput
             name="title"
+            value={link.title}
+            onChange={e => setLink({ ...link, title: e.target.value })}
             required={true}
             placeHolder="Enter title"
             externalStyle="mt-8 shadow-sm"
           />
           <TextInput
             name="url"
+            value={link.url}
+            onChange={e => setLink({ ...link, url: e.target.value })}
             required={true}
             placeHolder="Enter URL link"
             externalStyle="mt-4 shadow-sm"
           />
-          <div>
-            <Button
-              type="Submit"
-              text="Submit"
-              onClick={() => setShowAddNewLink(false)}
-              externalStyle="mt-16"
-            />
+          <div className="flex flex-col sm:flex-row mt-16">
+            <Button type="submit" text="Submit" onClick={closeAllFormView} />
             <Button
               type="button"
               text="Cancel"
-              onClick={() => setShowAddNewLink(false)}
-              externalStyle="mt-8 sm:ml-8"
+              onClick={closeAllFormView}
+              externalStyle="mt-8 sm:mt-0 sm:ml-8"
             />
           </div>
         </form>
@@ -70,7 +77,10 @@ function Profile() {
 
   const [showRedirect, setShowRedirect] = useState(false);
   const [redirectUrl, setRedirectUrl] = useState('');
-  const [showAddNewLink, setShowAddNewLink] = useState(false);
+
+  const [showAddNewLinkForm, setShowAddNewLinkForm] = useState(false);
+  const [showEditLinkForm, setShowEditLinkForm] = useState(false);
+  const [link, setLink] = useState({ title: '', url: '' });
 
   const user = {
     firstName: 'John',
@@ -87,8 +97,15 @@ function Profile() {
     setShowRedirect(true);
   };
 
-  const onClickAddNewLink = () => {
-    setShowAddNewLink(true);
+  const onClickEditLink = link => {
+    setShowEditLinkForm(true);
+    setLink(link);
+  };
+
+  const closeAllFormView = () => {
+    setShowAddNewLinkForm(false);
+    setShowEditLinkForm(false);
+    setLink({ title: '', url: '' });
   };
 
   return (
@@ -100,14 +117,29 @@ function Profile() {
             setShowRedirect={setShowRedirect}
           />
         )}
-        {showAddNewLink && <AddNewLink setShowAddNewLink={setShowAddNewLink} />}
+        {showAddNewLinkForm && (
+          <LinkForm
+            link={link}
+            setLink={setLink}
+            formFor="new"
+            closeAllFormView={closeAllFormView}
+          />
+        )}
+        {showEditLinkForm && (
+          <LinkForm
+            link={link}
+            setLink={setLink}
+            formFor="update"
+            closeAllFormView={closeAllFormView}
+          />
+        )}
         {isLoggedIn ? (
           <>
             <div className="sm:flex justify-between">
               <h1 className="text-4xl">Hi {user.firstName}!</h1>
               <Button
                 text="Add new link"
-                onClick={onClickAddNewLink}
+                onClick={() => setShowAddNewLinkForm(true)}
                 externalStyle="mt-4 sm:mt-0"
               />
             </div>
@@ -120,13 +152,24 @@ function Profile() {
         )}
         <div className="mt-8 mx-auto w-full sm:min-w-[400px] sm:w-fit flex flex-col">
           {user.links.map((link, index) => (
-            <Button
+            <div
               key={'profile_links_' + index}
-              type="button"
-              externalStyle="mt-4"
-              text={link.title}
-              onClick={() => onClickLink(link.url)}
-            />
+              className="mt-4 flex items-center"
+            >
+              <Button
+                type="button"
+                externalStyle="w-full border border-primary"
+                text={link.title}
+                onClick={() => onClickLink(link.url)}
+              />
+              {isLoggedIn && (
+                <IconButton
+                  icon={{ icon: edit, name: 'edit icon' }}
+                  className="ml-2 bg-white"
+                  onClick={() => onClickEditLink(link)}
+                />
+              )}
+            </div>
           ))}
         </div>
       </div>
--- frontend/src/components/TextInput.js
+++ frontend/src/components/TextInput.js
@@ -1,6 +1,8 @@
 function TextInput({
   type = 'text',
   name,
+  value,
+  onChange,
   required = false,
   placeHolder = '',
   externalStyle,
@@ -9,6 +11,8 @@ function TextInput({
     <input
       type={type}
       name={name}
+      value={value}
+      onChange={onChange}
       required={required}
       placeholder={placeHolder}
       className={`min-w-[300px] bg-secondary py-3 rounded-lg outline-none text-center text-primary placeholder:text-center placeholder:text-primary placeholder:text-opacity-50 ${externalStyle}`}
