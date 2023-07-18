0b435a6c48b552991f2f5c041be2d9715900eb3d
Mohd Ahmed Abdus <mdahmed.abdus@gmail.com>
2023-07-18 12:54:12 +0530

    Add username and iss to jwt

--- backend/services/authService.js
+++ backend/services/authService.js
@@ -1,12 +1,13 @@
 const jwt = require('jsonwebtoken');
 const { JWT_PRIVATE_KEY } = require('../config/keys');
 const { AUTH_MAX_AGE_IN_SECONDS } = require('../config/authConfig');
-const { Unauthorized } = require('../errors/customErrors');
 const { User } = require('../models/User');
+const { APP_URL } = require('../config/appConfig');
 
 const login = (req, res, user) =>
-  jwt.sign({ userId: user._id }, JWT_PRIVATE_KEY, {
+  jwt.sign({ userId: user._id, username: user.username }, JWT_PRIVATE_KEY, {
     expiresIn: AUTH_MAX_AGE_IN_SECONDS,
+    issuer: APP_URL,
   });
 
 const isLoggedIn = req => {
