905b33ec251b6d06f6f23e6aed70473fa007a550
Mohd Ahmed Abdus <mdahmed.abdus@gmail.com>
2023-07-18 12:19:43 +0530

    Add cors

--- backend/middleware/appMiddleware.js
+++ backend/middleware/appMiddleware.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const { IN_PROD } = require('../config/appConfig');
 
 module.exports = app => {
   // body parser
+  app.use(cors());
   app.use(express.json());
   app.use(cookieParser());
 
