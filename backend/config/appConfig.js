45c90c1924c9f7fef967cb5615d4db3026b87d34
Mohd Ahmed Abdus <mdahmed.abdus@gmail.com>
2023-08-07 12:39:26 +0530

    WIP: correct app url

--- backend/config/appConfig.js
+++ backend/config/appConfig.js
@@ -1,12 +1,12 @@
-const { NODE_ENV, APP_HOSTNAME, APP_PORT } = process.env;
+const { NODE_ENV, APP_HOSTNAME, APP_PORT, RENDER_EXTERNAL_URL } = process.env;
 
 const IN_PROD = NODE_ENV === 'production';
 
 const APP_PROTOCOL = IN_PROD ? 'https' : 'http';
 
-const APP_URL = `${APP_PROTOCOL}://${APP_HOSTNAME}:${
-  IN_PROD ? APP_PORT : 3000
-}`;
+const APP_URL = IN_PROD
+  ? RENDER_EXTERNAL_URL
+  : `${APP_PROTOCOL}://${APP_HOSTNAME}:${APP_PORT}`;
 
 module.exports = {
   NODE_ENV,
