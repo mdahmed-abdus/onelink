ce41c264092f13e89f7eb493452fcf9b84bb25ee
Mohd Ahmed Abdus <mdahmed.abdus@gmail.com>
2023-08-09 15:37:49 +0530

    Use smtp for dev and resend for prod

--- backend/services/mailService.js
+++ backend/services/mailService.js
@@ -6,6 +6,7 @@ const {
   RESEND_API_KEY,
   RESEND_FROM,
 } = require('../config/mailConfig');
+const { IN_PROD } = require('../config/appConfig');
 
 // smtp
 const transporter = nodemailer.createTransport(SMTP_OPTIONS);
@@ -16,7 +17,10 @@ const smtpSendMail = options =>
 // resend
 const resend = new Resend(RESEND_API_KEY);
 
-const sendMail = options =>
+const resendSendMail = options =>
   resend.emails.send({ ...options, from: RESEND_FROM });
+
+// use smtp for development and resend for production
+const sendMail = IN_PROD ? resendSendMail : smtpSendMail;
 
 module.exports = { smtpSendMail, sendMail };
