a6051aab0f65c07229a16abe639c6abea97e1ed6
Mohd Ahmed Abdus <mdahmed.abdus@gmail.com>
2023-08-10 18:43:04 +0530

    Add email template for sending demo user details

--- backend/services/mailService.js
+++ backend/services/mailService.js
@@ -23,4 +23,19 @@ const resendSendMail = options =>
 // use smtp for development and resend for production
 const sendMail = IN_PROD ? resendSendMail : smtpSendMail;
 
-module.exports = { smtpSendMail, sendMail };
+// templates
+const demoUserCredentialsTemplate = demoUser => `
+Use given credentials to login as a demo user:
+
+First name - ${demoUser.firstName}
+Last name - ${demoUser.lastName}
+Email - ${demoUser.email}
+Username - ${demoUser.username}
+Password - ${demoUser.password}
+    
+Note that the demo account and data associated will be deleted after 24 hours.
+    
+Please feel free to reach out on Twitter (now X) or email if you have any questions.
+`;
+
+module.exports = { smtpSendMail, sendMail, demoUserCredentialsTemplate };
